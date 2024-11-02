5ff8922c493efe8615596e41107c781c53a52ab2
Vicente Arce <vicente.arce@ug.uchile.cl>
2024-11-02 14:25:17 -0300

    feat: implemented the double dispatch method to apply effects of attack for each unit type (they function differently).

--- src/main/scala/model/actions/base/Attack.scala
+++ src/main/scala/model/actions/base/Attack.scala
@@ -1,11 +1,23 @@
 package model.actions.base
 
+import api.{Source, Target}
 import model.actions.AbstractAction
+import model.exceptions.*
+import model.units.Enemy
+import model.units.characters.Character
 import spray.json.JsObject
 import util.Json.{*, given}
 
 class Attack(name: String) extends AbstractAction(name){
-  
+
+  override def characterExecute(character: Character, tgt: Target): Unit = {
+    tgt.beAttacked(character.getWeapon.getOrElse(throw new WeaponNotPresent(character)).getTotalAttack)
+  }
+
+  override def enemyExecute(enemy: Enemy, tgt: Target): Unit = {
+    tgt.beAttacked(enemy.getDp)
+  }
+
   override def id: String = "Attack" // Identifier for this action
 
   // Converts the action and its details to a JSON object
