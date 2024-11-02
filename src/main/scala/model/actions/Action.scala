134238b5ea39584efcce300362684d1cfcf580d3
Vicente Arce <vicente.arce@ug.uchile.cl>
2024-11-02 14:24:12 -0300

    feat: added double dispatch method to disambiguate each unit type in the Action class.

--- src/main/scala/model/actions/Action.scala
+++ src/main/scala/model/actions/Action.scala
@@ -1,12 +1,16 @@
 package model.actions
 
 import api.{Source, Target}
+import model.units.Enemy
+import model.units.characters.Character
 
 // Trait defining basic actions with a name
 trait Action {
   def setName(name: String): Unit // Sets the action's name
   
   def getName: String // Retrieves the action's name
-  
-  def apply(src: Source, tgt: Target): Unit 
+
+  def enemyExecute(enemy: Enemy, tgt: Target): Unit
+
+  def characterExecute(character: Character, tgt: Target): Unit
 }
