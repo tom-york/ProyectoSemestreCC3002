3b4006e2ab7a0d1e9eefec0a5383cefb6a00abc3
Vicente Arce <vicente.arce@ug.uchile.cl>
2024-11-02 20:03:50 -0300

    refactor: Changes to the import order and syntax.

--- src/main/scala/model/units/characters/magic/BlackWizard.scala
+++ src/main/scala/model/units/characters/magic/BlackWizard.scala
@@ -1,15 +1,14 @@
 package model.units.characters.magic
 
-import model.items.Item
-import model.items.weapons.Weapon
 import model.units.characters.AbstractMagicCharacter
 import model.actions.Action
 import model.actions.base.*
 import model.actions.spells.black.*
 import model.actions.usage.*
+import model.items.Item
 import model.items.weapons.normal.Dagger
 import model.items.weapons.magic.*
-
+import model.items.weapons.Weapon
 
 class BlackWizard(name: String, healthPoints: Int, defensePoints: Int, weight: Int, weaponSlot: Option[Weapon], itemInventory: List[Item], manaPoints: Int) extends AbstractMagicCharacter(name, healthPoints, defensePoints, weight, weaponSlot, itemInventory, manaPoints) {
   private val compatibleWeapons: List[Weapon] = List(new Dagger(this), new Wand(this), new Staff(this))
--- src/main/scala/model/units/characters/magic/WhiteWizard.scala
+++ src/main/scala/model/units/characters/magic/WhiteWizard.scala
@@ -1,14 +1,14 @@
 package model.units.characters.magic
 
-import model.items.Item
-import model.items.weapons.Weapon
 import model.units.characters.AbstractMagicCharacter
 import model.actions.Action
 import model.actions.base.*
 import model.actions.spells.white.*
 import model.actions.usage.*
+import model.items.Item
 import model.items.weapons.normal.Bow
 import model.items.weapons.magic.*
+import model.items.weapons.Weapon
 
 class WhiteWizard(name: String, healthPoints: Int, defensePoints: Int, weight: Int, weaponSlot: Option[Weapon], itemInventory: List[Item], manaPoints: Int) extends AbstractMagicCharacter(name, healthPoints, defensePoints, weight, weaponSlot, itemInventory, manaPoints) {
   private val compatibleWeapons: List[Weapon] = List(new Bow(this), new Wand(this), new Staff(this))
