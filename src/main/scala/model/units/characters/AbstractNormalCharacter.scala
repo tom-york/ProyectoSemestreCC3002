c622766b75bcf13d5d2b7e8ffece13b4d4d3dd1d
Vicente Arce <vicente.arce@ug.uchile.cl>
2024-11-02 19:21:33 -0300

    feat: Added compatible potions to normal characters, and modified extends to the new abstract class.

--- src/main/scala/model/units/characters/AbstractNormalCharacter.scala
+++ src/main/scala/model/units/characters/AbstractNormalCharacter.scala
@@ -1,5 +1,11 @@
 package model.units.characters
+import model.items.Item
+import model.items.potions.Potion
+import model.items.potions.normal.*
+import model.items.weapons.Weapon
 
-abstract class AbstractNormalCharacter {
+abstract class AbstractNormalCharacter(name: String, healthPoints: Int, defensePoints: Int, weight: Int, weaponSlot: Option[Weapon], itemInventory: List[Item]) extends AbstractCharacter(name, healthPoints, defensePoints, weight, weaponSlot, itemInventory) {
+
+  protected val compatibleConsumables: List[Potion] = List(new HealingPotion("HealPot"), new StrengthPotion("StrengthPot"))
 
 }
--- src/main/scala/model/units/characters/normal/Thief.scala
+++ src/main/scala/model/units/characters/normal/Thief.scala
@@ -2,8 +2,8 @@ package model.units.characters.normal
 
 import model.items.Item
 import model.items.weapons.Weapon
-import model.units.characters.AbstractCharacter
+import model.units.characters.AbstractNormalCharacter
 
-class Thief(name: String, healthPoints: Int, defensePoints: Int, weight: Int, weaponSlot: Option[Weapon], itemInventory: List[Item]) extends AbstractCharacter(name, healthPoints, defensePoints, weight, weaponSlot, itemInventory) {
+class Thief(name: String, healthPoints: Int, defensePoints: Int, weight: Int, weaponSlot: Option[Weapon], itemInventory: List[Item]) extends AbstractNormalCharacter(name, healthPoints, defensePoints, weight, weaponSlot, itemInventory) {
   
 }
