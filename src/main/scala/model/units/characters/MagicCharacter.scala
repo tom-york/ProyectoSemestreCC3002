2acb643ebe35b48d8d878ab6a02a38c23a229cf5
Vicente Arce <vicente.arce@ug.uchile.cl>
2024-11-03 23:12:00 -0300

    fix: fixed the MagicCharacter extend from Units to Character

--- src/main/scala/model/units/characters/MagicCharacter.scala
+++ src/main/scala/model/units/characters/MagicCharacter.scala
@@ -3,7 +3,7 @@ package model.units.characters
 import model.units.Units
 
 // Trait defining magic-specific behavior for characters with mana points
-trait MagicCharacter extends Units {
+trait MagicCharacter extends Character {
 
   def setMp(manaP: Int): Unit  // Set the character's mana points
   def getMp: Int  // Get the character's mana points
