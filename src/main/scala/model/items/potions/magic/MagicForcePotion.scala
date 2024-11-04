7c17633970376a030acb1df9229eff3d642a45ce
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-04 17:47:23 -0300

    refactor: Renamed the methods defined in this potion to match the renamed declarations in the trait.

--- src/main/scala/model/items/potions/magic/MagicForcePotion.scala
+++ src/main/scala/model/items/potions/magic/MagicForcePotion.scala
@@ -3,15 +3,15 @@ package model.items.potions.magic
 import model.exceptions.InvalidSourceTarget
 import model.items.potions.AbstractPotion
 import model.units.Units
-import model.units.characters.MagicCharacter
+import model.units.characters.{Character, MagicCharacter}
 import util.Json.{*, given}
 
 class MagicForcePotion(name: String) extends AbstractPotion(name) {
-  override def unitConsume(unit: Units): Unit = {
-    throw new InvalidSourceTarget(unit.getName, "Magic Force Potion")
+  override def characterConsume(character: Character): Unit = {
+    throw new InvalidSourceTarget(character.getName, "Magic Force Potion")
   }
 
-  override def unitMagicConsume(character: MagicCharacter): Unit = {
+  override def magicCharacterConsume(character: MagicCharacter): Unit = {
     character.magicForcePotionEffect()
   }
 }
