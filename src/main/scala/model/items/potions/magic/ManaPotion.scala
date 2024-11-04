4f278299cac39cb5289580c1e4a020e8937543ac
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-04 17:49:50 -0300

    feat: Defined the application of the effects of the Mana Potion.

--- src/main/scala/model/items/potions/magic/ManaPotion.scala
+++ src/main/scala/model/items/potions/magic/ManaPotion.scala
@@ -1,7 +1,24 @@
 package model.items.potions.magic
 
+import model.exceptions.InvalidSourceTarget
 import model.items.potions.AbstractPotion
+import model.units.Units
+import model.units.characters.{Character, MagicCharacter}
 
 class ManaPotion(name: String) extends AbstractPotion(name) {
-  
+  override def characterConsume(character: Character): Unit = {
+    throw new InvalidSourceTarget(character.getName, "Mana Potion")
+  }
+
+  override def magicCharacterConsume(character: MagicCharacter): Unit = {
+    val currentMP: Int  = character.getMp
+    val maxMP: Int  = character.getMaxMp
+    val calculateEffects: Float = currentMP + Math.round(maxMP * 0.3)
+    if (Math.round(calculateEffects) < maxMP) {
+      character.setMp(Math.round(calculateEffects))
+    }
+    else {
+      character.setMp(maxMP)
+    }
+  }
 }
