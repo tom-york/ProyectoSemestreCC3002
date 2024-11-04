47d5967d6ca9bdfa923b9a5180d259ec21409da7
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-04 02:51:41 -0300

    refactor: New method defined to handle the consume action for magic characters.

--- src/main/scala/api/Target.scala
+++ src/main/scala/api/Target.scala
@@ -1,9 +1,11 @@
 package api
 
 import model.units.Units
+import model.units.characters.MagicCharacter
 
 trait Target extends GameObject {
   def beAttacked(attackDmg: Int): Unit
   def moveUnit(unit: Units): Unit
   def unitConsume(unit: Units): Unit
+  def unitMagicConsume(character: MagicCharacter): Unit
 }
