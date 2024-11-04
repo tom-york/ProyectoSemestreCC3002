490d1d3282f1d9045a6299b70c5adb2f4aeaaaf0
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-04 03:18:50 -0300

    feat: New exception for invalid Source for a certain Action.

---
+++ src/main/scala/model/exceptions/InvalidSourceAction.scala
@@ -0,0 +1,5 @@
+package model.exceptions
+
+class InvalidSourceAction(sourceName: String, actionName: String) extends Exception(s"${sourceName} is an invalid source for the action ${actionName}") {
+
+}
