64c0141476f617387ee9a110167a0bc59187a5a5
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-04 17:50:11 -0300

    feat: Defined the application of the effects of the Healing Potion.

--- src/main/scala/model/items/potions/normal/HealingPotion.scala
+++ src/main/scala/model/items/potions/normal/HealingPotion.scala
@@ -1,7 +1,23 @@
 package model.items.potions.normal
 
 import model.items.potions.AbstractPotion
+import model.units.Units
+import model.units.characters.{Character, MagicCharacter}
 
 class HealingPotion(name: String) extends AbstractPotion(name) {
-  
+  override def characterConsume(character: Character): Unit = {
+    val currentHP: Int  = character.getHp
+    val maxHP: Int  = character.getMaxHp
+    val calculateEffects: Float = currentHP + Math.round(maxHP * 0.2)
+    if (Math.round(calculateEffects) < maxHP) {
+      character.setHp(Math.round(calculateEffects))
+    }
+    else {
+      character.setHp(maxHP)
+    }  
+  }
+
+  override def magicCharacterConsume(character: MagicCharacter): Unit = {
+    characterConsume(character)
+  }
 }
