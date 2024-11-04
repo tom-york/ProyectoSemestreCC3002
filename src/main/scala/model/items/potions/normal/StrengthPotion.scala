1f22dbfb0a58c59cc0ca382c5e694cf0afc69693
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-04 19:42:24 -0300

    feat: Implemented the effects of the Strength Potion.

--- src/main/scala/model/items/potions/normal/StrengthPotion.scala
+++ src/main/scala/model/items/potions/normal/StrengthPotion.scala
@@ -1,7 +1,16 @@
 package model.items.potions.normal
 
 import model.items.potions.AbstractPotion
+import model.units.Units
+import model.units.characters.{Character, MagicCharacter}
 
 class StrengthPotion(name: String) extends AbstractPotion(name) {
-  
+  override def characterConsume(character: Character): Unit = {
+    val multiplier: Float = 1.15
+    character.setDp(Math.round(character.getDp * multiplier))
+  }
+
+  override def magicCharacterConsume(character: MagicCharacter): Unit = {
+    characterConsume(character)
+  }
 }
