479acee09b6aea6772a74452d18ffa56731657cd
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-04 03:26:38 -0300

    feat: Implementation of the effects of the MagicForcePotion.

--- src/main/scala/model/items/potions/magic/MagicForcePotion.scala
+++ src/main/scala/model/items/potions/magic/MagicForcePotion.scala
@@ -1,8 +1,17 @@
 package model.items.potions.magic
 
+import model.exceptions.InvalidSourceTarget
 import model.items.potions.AbstractPotion
+import model.units.Units
+import model.units.characters.MagicCharacter
 import util.Json.{*, given}
 
 class MagicForcePotion(name: String) extends AbstractPotion(name) {
-  
+  override def unitConsume(unit: Units): Unit = {
+    throw new InvalidSourceTarget(unit.getName, "Magic Force Potion")
+  }
+
+  override def unitMagicConsume(character: MagicCharacter): Unit = {
+    character.magicForcePotionEffect()
+  }
 }
