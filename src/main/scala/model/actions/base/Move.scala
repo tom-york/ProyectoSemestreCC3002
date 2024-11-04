60d052d1e7191a4305a11d7323917fa0114444a4
Vicente Arce <vicente.arce@ug.uchile.cl>
2024-11-03 23:12:41 -0300

    feat: Implemented the double dispatch methods for this action.

--- src/main/scala/model/actions/base/Move.scala
+++ src/main/scala/model/actions/base/Move.scala
@@ -1,10 +1,24 @@
 package model.actions.base
 
+import api.Target
 import model.actions.AbstractAction
+import model.units.Enemy
+import model.units.characters.{Character, MagicCharacter}
 import util.Json.{*, given}
 
 class Move(name: String = "Move") extends AbstractAction(name) {
+  override def normalCharacterExecute(character: Character, tgt: Target): Unit = {
+    tgt.moveUnit(character)
+  }
 
+  override def magicCharacterExecute(character: MagicCharacter, tgt: Target): Unit = {
+    tgt.moveUnit(character)
+  }
+
+  override def enemyExecute(enemy: Enemy, tgt: Target): Unit = {
+    tgt.moveUnit(enemy)
+  }
+  
   override def id: String = "2" // Identifier for this action
 
   // Converts the action and its details to a JSON object
