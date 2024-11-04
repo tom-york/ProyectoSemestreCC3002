4b50da7aa00ad2b835afa9a2ab503a784caa2c5d
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-04 17:45:09 -0300

    refactor: Modified the names of the consumption methods for better readability.

--- src/main/scala/api/Target.scala
+++ src/main/scala/api/Target.scala
@@ -1,11 +1,11 @@
 package api
 
 import model.units.Units
-import model.units.characters.MagicCharacter
+import model.units.characters.{Character, MagicCharacter}
 
 trait Target extends GameObject {
   def beAttacked(attackDmg: Int): Unit
   def moveUnit(unit: Units): Unit
-  def unitConsume(unit: Units): Unit
-  def unitMagicConsume(character: MagicCharacter): Unit
+  def characterConsume(character: Character): Unit
+  def magicCharacterConsume(character: MagicCharacter): Unit
 }
