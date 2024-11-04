94aae0961ff5991d3d76c706bd005b4046178d91
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-04 03:19:53 -0300

    style: Remove brackets inside the exception string.

--- src/main/scala/model/exceptions/InvalidActionTarget.scala
+++ src/main/scala/model/exceptions/InvalidActionTarget.scala
@@ -2,6 +2,6 @@ package model.exceptions
 
 import model.actions.Action
 
-class InvalidActionTarget(targetName: String, actionName: String) extends Exception(s"${targetName} is an invalid target for the action ${actionName}") {
+class InvalidActionTarget(targetName: String, actionName: String) extends Exception(s"$targetName is an invalid target for the action $actionName") {
 
 }
--- src/main/scala/model/exceptions/InvalidSourceTarget.scala
+++ src/main/scala/model/exceptions/InvalidSourceTarget.scala
@@ -1,5 +1,5 @@
 package model.exceptions
 
-class InvalidSourceTarget(sourceName: String, targetName: String) extends Exception(s"The source ${targetName} isn't compatible with the target ${sourceName}"){
+class InvalidSourceTarget(sourceName: String, targetName: String) extends Exception(s"The source $targetName isn't compatible with the target $sourceName"){
 
 }
