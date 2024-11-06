df74810e0a2eca393f188066a6b54fa399507090
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-06 11:54:43 -0300

    docs: Improved documentation for classes and traits for panels.

--- src/main/scala/model/panels/IPanel.scala
+++ src/main/scala/model/panels/IPanel.scala
@@ -5,27 +5,76 @@ import model.units.Units
 
 import scala.collection.mutable.ArrayBuffer
 
-// Interface defining a panel in the game, extending GameObject
+/**
+ * Interface defining a panel in the game, representing a location where units can be positioned.
+ * Extends the GameObject trait to inherit basic game object properties.
+ */
 trait IPanel extends GameObject {
-  def setCoordinates(x: Int, y: Int): Unit // Sets the coordinates of the panel
 
-  def setUnits(uArray: ArrayBuffer[Units]): Unit // Sets the units present on the panel
+  /**
+   * Sets the coordinates of the panel.
+   * @param x the x-coordinate of the panel
+   * @param y the y-coordinate of the panel
+   */
+  def setCoordinates(x: Int, y: Int): Unit
 
-  def setNorth(panel: Option[Panel]): Unit // Sets the northern neighbor panel
+  /**
+   * Sets the units present on the panel.
+   * @param uArray an ArrayBuffer of Units to place on the panel
+   */
+  def setUnits(uArray: ArrayBuffer[Units]): Unit
 
-  def setWest(panel: Option[Panel]): Unit // Sets the western neighbor panel
+  /**
+   * Sets the northern neighboring panel.
+   * @param panel an Option containing the panel to the north, or None if there is no neighbor
+   */
+  def setNorth(panel: Option[Panel]): Unit
 
-  def setSouth(panel: Option[Panel]): Unit // Sets the southern neighbor panel
+  /**
+   * Sets the western neighboring panel.
+   * @param panel an Option containing the panel to the west, or None if there is no neighbor
+   */
+  def setWest(panel: Option[Panel]): Unit
 
-  def setEast(panel: Option[Panel]): Unit // Sets the eastern neighbor panel
+  /**
+   * Sets the southern neighboring panel.
+   * @param panel an Option containing the panel to the south, or None if there is no neighbor
+   */
+  def setSouth(panel: Option[Panel]): Unit
 
-  def getCoordinates: (Int, Int) // Retrieves the coordinates of the panel
+  /**
+   * Sets the eastern neighboring panel.
+   * @param panel an Option containing the panel to the east, or None if there is no neighbor
+   */
+  def setEast(panel: Option[Panel]): Unit
 
-  def getUnits: ArrayBuffer[Units] // Retrieves the units on the panel
+  /**
+   * Retrieves the coordinates of the panel.
+   * @return a tuple containing the x and y coordinates
+   */
+  def getCoordinates: (Int, Int)
 
-  def getNeighbours: List[Panel] // Retrieves the northern neighbor panel
-  
+  /**
+   * Retrieves the units currently on the panel.
+   * @return an ArrayBuffer of Units on the panel
+   */
+  def getUnits: ArrayBuffer[Units]
+
+  /**
+   * Retrieves the list of neighboring panels (north, west, south, east).
+   * @return a list of neighboring panels in a specific order: [north, west, south, east]
+   */
+  def getNeighbours: List[Panel]
+
+  /**
+   * Adds a unit to the panel.
+   * @param unit the unit to add to the panel
+   */
   def addUnit(unit: Units): Unit
-  
+
+  /**
+   * Removes a unit from the panel.
+   * @param unit the unit to remove from the panel
+   */
   def removeUnit(unit: Units): Unit
 }
