f6cd550c6447c519a5b308c03fc1fa84eba187c9
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-06 10:49:19 -0300

    feat: Initial implementation for the Game Controller methods.

--- src/main/scala/controller/GameController.scala
+++ src/main/scala/controller/GameController.scala
@@ -1,15 +1,34 @@
 package controller
 
-import api.{GameApi, IGameController}
+import api.{GameApi, IGameController, Source}
+import model.actions.Action
+import model.exceptions.NoIdFound
+import model.panels.Panel
 import model.players.Player
 import model.scheduler.TaskScheduler
+import model.units.Units
 import util.Json.{*, given}
+
+import scala.collection.mutable.ArrayBuffer
 
 object GameController extends IGameController {
   /* write your code here */
   private val gameScheduler: TaskScheduler = new TaskScheduler()
   private val player1: Player = new Player(List())
   private val player2: Player = new Player(List())
+  private val panel1: Panel = new Panel((1,1), ArrayBuffer())
+  private val panel2: Panel = new Panel((1,2), ArrayBuffer())
+  private val panel3: Panel = new Panel((2,1), ArrayBuffer())
+  private val panel4: Panel = new Panel((2,2), ArrayBuffer())
+  panel1.setSouth(Some(panel3))
+  panel1.setEast(Some(panel2))
+  panel2.setWest(Some(panel1))
+  panel2.setSouth(Some(panel4))
+  panel3.setNorth(Some(panel1))
+  panel3.setEast(Some(panel4))
+  panel4.setNorth(Some(panel2))
+  panel4.setWest(Some(panel3))
+
   /** Returns the players of the game. In this version of the project, there are
    * strictly two players. Each player should send the information of his game
    * units.
@@ -25,7 +44,12 @@ object GameController extends IGameController {
    * units will be interacting with.
    */
   def getGamePanels: JsVal = {
-    JsArr()
+    JsArr(
+      panel1.toJson,
+      panel2.toJson,
+      panel3.toJson,
+      panel4.toJson
+    )
   }
 
   /** Returns the id of the current game unit. The current game unit is the unit
@@ -37,7 +61,12 @@ object GameController extends IGameController {
   /** Decides the next game unit id. This method is called after an action is
    * executed. It should return the id of the next game unit that will play.
    */
-  def decideNextGameUnitId: String = "placeholder"
+  def decideNextGameUnitId: String = {
+    while (gameScheduler.getCompleteActionBarUnits.isEmpty) {
+      gameScheduler.increaseActionBars(1)
+    }
+    gameScheduler.getCompleteActionBarUnits.head.id
+  }
 
   /** Returns the actions that a character can do. It receives the id of the
    * character and returns the actions that the character can do.
@@ -48,7 +77,19 @@ object GameController extends IGameController {
    * the actions that the game unit can do, or None if the game unit is not
    * found
    */
-  def findActionsByGameUnitId(id: String): Option[JsVal] = None
+  def getUnitById(id: String): Units = {
+    val gameUnits: List[Units] = gameScheduler.getSchedulerContent.keys.toList
+    gameUnits.find(_.getId == id).getOrElse(
+      throw new NoIdFound(id)
+    )
+  }
+
+  def findActionsByGameUnitId(id: String): Option[JsVal] = {
+    val gameUnit: Units = getUnitById(id)
+    val actionsJson = JsArr(gameUnit.actions.map(_.toJson))
+
+    Some(actionsJson)
+  }
 
   /** Executes an action.
    *
@@ -61,7 +102,18 @@ object GameController extends IGameController {
    * @return
    * a message that describes the result of the action
    */
-  def doAction(actionId: String, sourceId: String, targetId: String): String = "placeholder"
+  def doAction(actionId: String, sourceId: String, targetId: String): String = {
+    try {
+      val source: Units = getUnitById(sourceId)
+      val action: Action = source.findActionById(actionId)
+      val target: Units = getUnitById(targetId)
+      source.doAction(action, target)
+      "Action successful"
+    } catch {
+      //Placeholder error, in next releases will be changed for each individual exception
+      case e: Exception => "Error"
+    }
+  }
 
   /** Resets the game. All configurations and parameters should be reset too. */
   def reset(): Unit = None
