6799ac36cb8a9e3a38bc38980c21ee5765e88182
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-11-06 02:32:47 -0300

    test: Created tests for the consumption of all potions.

--- src/test/scala/model/units/DoActionTest.scala
+++ src/test/scala/model/units/DoActionTest.scala
@@ -1,8 +1,10 @@
 package model.units
 
 import model.actions.base.{Attack, Move}
-import model.items.potions.magic.ManaPotion
-import model.items.potions.normal.StrengthPotion
+import model.actions.spells.black.Thunder
+import model.actions.usage.Consume
+import model.items.potions.magic.{MagicForcePotion, ManaPotion}
+import model.items.potions.normal.{HealingPotion, StrengthPotion}
 import model.items.weapons.magic.Wand
 import model.items.weapons.normal.Sword
 import model.panels.Panel
@@ -18,8 +20,10 @@ class DoActionTest extends FunSuite{
   private var weapon1: Sword = _
   private var weapon2: Wand = _
   private var enemy1: Enemy = _
-  private var mana1: ManaPotion = _
-  private var strength1: StrengthPotion = _
+  private val mana1: ManaPotion = new ManaPotion("Mana Potion")
+  private val magicForce1: MagicForcePotion = new MagicForcePotion("Magic Force Potion")
+  private val strength1: StrengthPotion = new StrengthPotion("Strength Potion")
+  private val healing1: HealingPotion = new HealingPotion("Healing Potion")
   private var panel1: Panel = _
   private var panel2: Panel = _
   private var panel3: Panel = _
@@ -54,10 +58,8 @@ class DoActionTest extends FunSuite{
     blackWizard1.doAction(new Attack(), knight1)
     assertEquals(knight1.getHp, 90)
   }
+
   test("Move a unit.") {
-    assertEquals(knight1.getPanel, panel1)
-    assertEquals(enemy1.getPanel, panel1)
-    assertEquals(blackWizard1.getPanel, panel1)
     knight1.doAction(new Move(), panel2)
     assertEquals(panel2.getUnits, ArrayBuffer(knight1))
     enemy1.doAction(new Move(), panel2)
@@ -67,5 +69,44 @@ class DoActionTest extends FunSuite{
     assertEquals(knight1.getPanel, panel2)
     assertEquals(enemy1.getPanel, panel2)
     assertEquals(blackWizard1.getPanel, panel2)
+  }
+
+  test("Consume a healing potion.") {
+    //test healing potion
+    knight1.setHp(10)
+    knight1.doAction(new Consume("Healing", List(healing1)), healing1)
+    assertEquals(knight1.getHp, 28)
+    //check no over-healing occurs
+    blackWizard1.setHp(80)
+    blackWizard1.doAction(new Consume("Healing", List(healing1)), healing1)
+    assertEquals(blackWizard1.getHp, 90)
+  }
+
+  test("Consume a strength potion.") {
+    //test strength potion
+    knight1.doAction(new Consume("Strength", List(strength1)), strength1)
+    assertEquals(knight1.getDp, 69)
+    blackWizard1.doAction(new Consume("Strength", List(strength1)), strength1)
+    assertEquals(blackWizard1.getDp, 69)
+  }
+
+  test("Consume a Magic Force potion.") {
+    //test magic force potion
+    blackWizard1.doAction(new Consume("Magic Force", List(magicForce1)), magicForce1)
+    assertEquals(blackWizard1.getMagicDamage, 45)
+    //check if it resets after using a spell
+    blackWizard1.doAction(new Thunder(), enemy1)
+    assertEquals(blackWizard1.getMagicDamage, 30)
+  }
+
+  test("Consume a Mana potion.") {
+    //test mana potion
+    blackWizard1.setMp(10)
+    blackWizard1.doAction(new Consume("Mana", List(mana1)), mana1)
+    assertEquals(blackWizard1.getMp, 19)
+    //check mana points aren't over the maximum
+    blackWizard1.setMp(22)
+    blackWizard1.doAction(new Consume("Mana", List(mana1)), mana1)
+    assertEquals(blackWizard1.getMp, 30)
   }
 }
