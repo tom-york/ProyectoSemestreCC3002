a24a7a2ffd8a368ad34748d16ad08d8bb8b98aea
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-12-04 13:57:08 -0300

    docs: Added missing documentation to Subject.

--- src/main/scala/model/patterns/observer/Subject.scala
+++ src/main/scala/model/patterns/observer/Subject.scala
@@ -2,11 +2,27 @@ package model.patterns.observer
 
 import scala.collection.mutable.ArrayBuffer
 
-abstract class Subject[T] extends ISubject[T]{
+/** Abstract base class implementing the Subject interface for the Observer pattern.
+ *
+ * Manages a collection of observers and provides default implementations
+ * for registering and notifying observers.
+ *
+ * @tparam T The type of data passed during notification
+ */
+abstract class Subject[T] extends ISubject[T] {
+  /** Internal collection of registered observers. */
   private val observers: ArrayBuffer[Observer[T]] = ArrayBuffer()
 
+  /** Registers an observer to the collection.
+   *
+   * @param o The observer to be added
+   */
   def registerObserver(o: Observer[T]): Unit = observers += o
 
+  /** Notifies all registered observers with the given response.
+   *
+   * @param response The data to pass to each observer
+   */
   def notifyObservers(response: T): Unit = {
     for (o <- observers) {
       o.update(this, response)
