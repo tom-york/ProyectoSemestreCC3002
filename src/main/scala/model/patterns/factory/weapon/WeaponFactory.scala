0cc5f812f21e866d630430d4d353271290ddc346
Vicente Arce <vicente_arce_loyola@hotmail.com>
2024-12-04 13:55:36 -0300

    docs: Added missing documentation to WeaponFactory.

--- src/main/scala/model/patterns/factory/weapon/WeaponFactory.scala
+++ src/main/scala/model/patterns/factory/weapon/WeaponFactory.scala
@@ -2,6 +2,15 @@ package model.patterns.factory.weapon
 
 import model.items.weapons.Weapon
 
+/** Factory trait for creating game weapons.
+ *
+ * Provides a method to instantiate a new Weapon.
+ *
+ */
 trait WeaponFactory {
+  /** Creates and returns a new weapon.
+   *
+   * @return A Weapon object
+   */
   def apply(): Weapon 
 }
